acccc4086574eb1b467f1902adc9d3895486333c
lrodri20 <luis_felipe_rodriguez@yahoo.com>
2019-02-15 19:23:44 -0500

    summary
    summary

---
+++ DatingApp.API/Migrations/20190216001654_mysqlinitials.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class mysqlinitials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Photos",
+                newName: "URL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "URL",
+                table: "Photos",
+                newName: "Url");
+        }
+    }
+}
